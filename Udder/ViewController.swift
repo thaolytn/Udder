2abf62fad33c927a843b8e8c5ceebc0d4a10403b
Thaoly Ngo <thaoly.ngo@gmail.com>
2022-12-07 01:32:22 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Thaoly Ngo on 12/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
